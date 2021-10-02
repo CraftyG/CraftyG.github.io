5c1e9933dd1ab79a5750694de9417d8058d4676a
CraftyG <53879030+CraftyG@users.noreply.github.com>
2021-10-02 13:07:43 -0700

    Update makeElement.js

--- docs/makeElement.js
+++ docs/makeElement.js
@@ -3,3 +3,20 @@ function myFunction() {
   var btn = document.createElement("BUTTON");
   document.body.appendChild(btn);
 }
+
+// creating a div element
+function createDiv () {
+ var eDiv = document.createElement("Div");
+  eDiv.id = "divID";
+
+// Styling It
+  eDiv.style.textAlign = "center";
+  eDiv.style.fontWeight = "bold";
+  eDiv.style.fontSize = "smaller";
+  eDiv.style.paddingTop = "15px";
+ // Adds paragraph
+  var paragraph = document.createElement("p");
+  var text = document.createtextCode("A Styled Paragraph!!!");
+  paragraph.appendChild(text);
+  eDiv.appendChild(paragraph);
+}
