886c6570bad0a42cd5e7519c4431a55f420def86
CraftyG <53879030+CraftyG@users.noreply.github.com>
2021-10-17 13:29:00 -0700

    Update and rename makeElement.js to gameModal.js

---
+++ docs/gameModal.js
@@ -0,0 +1,20 @@
+// Get the Modal
+var modal = document.getElementById("gameModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("gameBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementByClassName("close)[0];
+                                          
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none;
+}
+
+// When ths user clicks anywhere outside the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none"
+  }
+}
