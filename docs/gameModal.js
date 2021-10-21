9ac220a1ec0016e0ac27f1efbbb9e1e7ad61e5b2
CraftyG <53879030+CraftyG@users.noreply.github.com>
2021-10-20 18:43:00 -0700

    Update gameModal.js

--- docs/gameModal.js
+++ docs/gameModal.js
@@ -1,25 +1,27 @@
-// Get the Modal
-var modal = document.getElementById("gameModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("gameBtn");
+var datamap = new Map([
+  [document.getElementById("gameBtn"), document.getElementById("gameModal")],
+  [document.getElementById("gameBtn2"), document.getElementById("gameModal2")]
+]);
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+datamap.forEach((value key) => {
+  doModal(key, value);
+});
 
-// When the user clicks on the button, open the modal;
-btn.onclick = function() {
-  modal.style.display = "block";  
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When ths user clicks anywhere outside the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+function doModal(anchor, popupbox) {
+  var span = popupbox.getElementsByClassName("close")[0];
+  
+  anchor.addEventListener("click", function (event) {
+    popupbox.style.display = "block";
+  });
+  
+   span.addEventListener("click", function (event) {
+   popupbox.style.display = "none";
+ });
+ 
+   window.addEventListener("click", function (event) {
+    if (event.target == popupbox) {
+     popupbox.style.display = "none";
+      }
+  });
 }
